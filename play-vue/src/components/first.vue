cba7530422700d7d43451c58131e79871a6bb70e
HL <1757363574@qq.com>
2018-08-10 17:26:14 +0800

    8.10-17
    添加了element框架等

---
+++ play-vue/src/components/first.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span>
+      &times;
+    </span>
+    <el-switch
+  v-model="value2"
+  active-color="blue"
+  inactive-color="#ff4949"
+  width='30'
+  height='20'
+  name='huangl'>
+</el-switch>
+  </div>
+</template>
+<script>
+export default {
+  name:'',
+  data () {
+    return {
+   value1: true,
+        value2: true
+    }
+  }
+  
+}
+</script>
+<style scoped>
+
+</style>
+
--- play-vue/src/router/index.js
+++ play-vue/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import first from '@/components/first'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'first',
+      component: first
     }
   ]
 })
