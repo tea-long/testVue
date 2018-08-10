72cf52b1f46d35453b40863e220908c61ab93bcc
HL <1757363574@qq.com>
2018-08-10 18:12:39 +0800

    8.10-18
    小修了下

--- play-vue/src/components/vue-todolist.vue
+++ play-vue/src/components/vue-todolist.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    
+
   </div>
 </template>
 <script>
 export default {
-  name:'',
-  data () {
-    return {
-
-    }
-  }
-  
-}
+      name: "",
+      data() {
+            return {};
+      }
+};
 </script>
 <style scoped>
-
 </style>
